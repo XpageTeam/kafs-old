2e02aaf9b9eeb2c6c195f0e3c49a5201e533dbd2
SergeyG <sergey1008@gmail.com>
2019-04-27 09:57:57 +0500

    git init

---
+++ src/js/common-new.js
@@ -0,0 +1,31 @@
+import $ from "jquery"
+import is from "is_js"
+
+
+window.$ = $;
+window.jQuery = $;
+
+
+
+
+document.addEventListener("DOMContentLoaded", e => {
+
+
+	
+})
+
+$(window).on("load resize", e => {
+	
+});
+
+$(window).on("load scroll resize", e => {
+
+
+
+
+	
+
+});
+
+
+
